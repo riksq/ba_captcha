c4bf756c9d83dc5531f964691dabd94f0212266a
Marina <golofaeva.xs4n@gmail.com>
2021-09-21 19:31:01 +0800

    123

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
+    // res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
+    return res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=9d9bda25" });
 }
