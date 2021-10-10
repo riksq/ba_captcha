647fafee95f7c9ec6b21b78ce9cda1c27d00f791
riksq <91621987+riksq@users.noreply.github.com>
2021-10-10 10:23:02 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 export default function handler(req, res) {
     // res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
-    return res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=9d9bda25" });
+    return res.status(200).json({ ok: true, url: "https://foxiv.top/stream/11050b5a-1f3e-426e-8086-db926d481c56" });
 }
