5a42193705e8183809e451cffc974fb66fd8d2de
riksq <91621987+riksq@users.noreply.github.com>
2022-03-25 11:57:46 -0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 export default function handler(req, res) {
     // res.status(200).json({ ok: true, url: "https://instaprix17.cam?a_aid=os&a_bid=ca7b841d" });
-    return res.status(200).json({ ok: true, url: "https://quiz.getfit.best/5Kz4qCmV" });
+    return res.status(200).json({ ok: true, url: "https://vk.goofitness.pro/m3NSn3FP" });
 }
